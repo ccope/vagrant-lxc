966455cf41a770f391886fd5a3a2d2dff8fcd0b7
Cam Cope <ccope@brightcove.com>
2014-01-02 18:22:29 -0500

    make cloning a part of the create action
    this allows you to provision VMs as normal in your Vagrantfile. I don't
    currently have a way to verify that the container being cloned exists
    though.

--- lib/vagrant-lxc/action/create.rb
+++ lib/vagrant-lxc/action/create.rb
@@ -7,16 +7,23 @@ module Vagrant
         end
 
         def call(env)
+          config = env[:machine].provider_config
+
           container_name = "#{env[:root_path].basename}_#{env[:machine].name}"
           container_name.gsub!(/[^-a-z0-9_]/i, "")
           container_name << "-#{Time.now.to_i}"
 
-          env[:machine].provider.driver.create(
-            container_name,
-            env[:lxc_template_src],
-            env[:lxc_template_config],
-            env[:lxc_template_opts]
-          )
+          if config.existing_container_name
+            env[:machine].provider.driver.clone(config.existing_container_name, container_name)
+          else
+            env[:machine].provider.driver.create(
+              container_name,
+              config.backingstore,
+              config.backingstore_options,
+              env[:lxc_template_src],
+              env[:lxc_template_config],
+              env[:lxc_template_opts])
+          end
 
           env[:machine].id = container_name
 
